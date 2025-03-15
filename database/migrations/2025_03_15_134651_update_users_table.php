1a4c49ece1d83c971df4a030aaf49efab6dff4ad
anouar el barry <elbarryanwar37@gmail.com>
2025-03-15 13:59:10 +0000

    update users table

---
+++ database/migrations/2025_03_15_134651_update_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users',function(Blueprint $table){
+           $table->enum('role',['admin','user'])->default('user');
+           $table->string('currency')->default('MAD');
+           $table->decimal('balance',10,2)->default(0);
+           $table->string('google_id')->nullable();
+           $table->string('facebook_id')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('role');
+            $table->dropColumn('currency');
+            $table->dropColumn('balance');
+            $table->dropColumn('google_id');
+            $table->dropColumn('facebook_id');
+        });
+    }
+};
