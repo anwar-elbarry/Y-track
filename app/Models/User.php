483458be11f182bc49940900b84cdca1a18c6f75
anouar el barry <elbarryanwar37@gmail.com>
2025-03-15 14:37:32 +0000

    add fillable

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,10 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'currency',
+        'balance',
+        'google_id',
+        'facebook_id',
     ];
 
     /**
