9b3405282d2665d2257f8dcdbb3c55f688ce01e4
anouar el barry <elbarryanwar37@gmail.com>
2025-03-15 16:22:59 +0000

    create redirect and callback methods for google

---
+++ app/Http/Controllers/Auth/GoogleAuthController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Laravel\Socialite\Facades\Socialite;
+
+class GoogleAuthController extends Controller
+{
+    public function redirect(){
+        return Socialite::driver('google')->redirect();
+    }
+
+    public function callback(){
+        try{
+           $googleUser = Socialite::driver('google')->user();
+           $user = User::where('google_id',$googleUser->id)->first();
+           
+           if(!$user){
+
+            $user = User::create([
+                'name' => $googleUser->getName(),
+                'email' => $googleUser->getEmail(),
+                'google_id' => $googleUser->getId(),
+                'email_verified_at' => now()
+            ]);
+           }
+           Auth::login($user);
+           return to_route('dashboard');
+        }catch(\Exception $e){
+            return to_route('signin')->with('error','Failed to authenticate with Google.');
+        }
+    }
+}
