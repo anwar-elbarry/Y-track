b8baef54634a4be8b2f5a0fa7e9a11210a8a2b6f
anouar el barry <elbarryanwar37@gmail.com>
2025-03-16 13:57:28 +0000

    make the footer responsive / add the icons in a links

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,7 +1,7 @@
 @extends('layoats.app')
 @section('main-content')
 <footer class="bg-[#061C3D] w-full overflow-hidden mt-[111px] pl-[202px] pr-48 max-md:max-w-full max-md:mt-10 max-md:px-5">
-      <div class="flex items-center gap-[40px_91px] justify-center  py-[83px] max-md:max-w-full">
+      <div class="flex items-center gap-[40px_91px] justify-center  py-[83px] lg:flex-nowrap flex-wrap max-md:max-w-full">
         <div class="self-stretch text-xs text-gray-400 font-normal leading-5 w-[210px] my-auto">
           <img
             src="{{ asset('images/Y-TRACK_light.svg') }}"
@@ -38,14 +38,14 @@
           </div>
         </div>
       </div>
-      <div class="justify-between items-center shadow-[0px_-0.672px_0px_0px_#243752] bg-[#061C3D] flex w-full max-w-[887px] gap-[40px_100px] flex-wrap py-[22px] max-md:max-w-full">
-    <div class="flex justify-evenly items-center w-33 gap-2 h-6">
-    <a  class="h-full w-full flex justify-center items-center" href="http://" target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-lg fa-discord" style="color: #ffffff; height: 100%; align-content: center;"></i></a>
-    <a  class="h-full w-full flex justify-center items-center" href="http://" target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-lg fa-facebook" style="color: #ffffff;height: 100%;align-content: center;"></i></a>    
-    <a  class="h-full w-full flex justify-center items-center" href="http://" target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-lg fa-square-x-twitter" style="color: #ffffff;height: 100%;align-content: center;"></i></a> 
-    <a  class="h-full w-full flex justify-center items-center" href="http://" target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-lg fa-github" style="color: #ffffff;height: 100%;align-content: center;"></i></a> 
+      <div class="justify-between items-center shadow-[0px_-0.672px_0px_0px_#243752] bg-[#061C3D] flex w-full gap-[40px_100px] flex-wrap py-5 max-md:max-w-full">
+    <div class="social_Footer flex justify-evenly items-center w-33 gap-2 h-6">
+    <a class="h-full w-full flex justify-center items-center" href="http://" target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-lg fa-discord" style=" height: 100%;align-content: center;"></i></a>
+    <a class="h-full w-full flex justify-center items-center" href="http://" target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-lg fa-facebook" style="height: 100%;align-content: center;"></i></a>    
+    <a class="h-full w-full flex justify-center items-center" href="http://" target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-lg fa-square-x-twitter" style="height: 100%;align-content: center;"></i></a> 
+    <a class="h-full w-full flex justify-center items-center" href="http://" target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-lg fa-github" style="height: 100%;align-content: center;"></i></a> 
     </div>
-        <div class="text-white text-center text-[11px] font-normal self-stretch my-auto">
+        <div class="text-white text-center text-[11px] font-normal self-stretch">
           © 2025 - Y-track
         </div>
       </div>
