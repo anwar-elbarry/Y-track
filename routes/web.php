740916b6dc81daba6a5ec06d949682838f06afee
anouar el barry <elbarryanwar37@gmail.com>
2025-04-08 16:10:02 +0100

    Update web routes

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-Route::get('/home', function () {
+Route::get('/', function () {
     return view('pages.home');
 })->name('home');
 Route::get('/aside',function(){
