390c525118e5e842d33a75f9f09a6b743de32f6d
anouar el barry <elbarryanwar37@gmail.com>
2025-04-10 16:57:13 +0100

    Update main.js to use Vue Router

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import router from './router';
 
 import { OhVueIcon, addIcons } from "oh-vue-icons";
 import { FaWallet, BiCashCoin , BiCalendarCheckFill , RiLineChartLine , GiOnTarget , RiFileTextFill , BiArrowLeft , BiArrowRight} from "oh-vue-icons/icons";
@@ -9,4 +10,10 @@ addIcons(FaWallet, BiCashCoin ,BiCalendarCheckFill , RiLineChartLine , GiOnTarge
 
 const app = createApp(App);
 app.component("v-icon", OhVueIcon);
+
+import Navbar from './components/Navbar.vue'
+app.component("navbar",Navbar);
+
+app.use(router)
+
 app.mount("#app");
