22ef68445e2acd4217c45434cef4e22963b9daef
anouar el barry <elbarryanwar37@gmail.com>
2025-04-12 22:15:00 +0100

    Add CreateGoal modal and state management to Goals page

--- frontend/src/views/Goals.vue
+++ frontend/src/views/Goals.vue
@@ -3,7 +3,7 @@
     <div class="up_content w-full justify-end items-center flex gap-5">
       <SearchBare
        title="Goal" />
-      <AddBtn name="Add Goal" />
+      <AddBtn name="Add Goal" @click="showCreateGoalForm = true"/>
     </div>
     <div class="goals_cards flex flex-wrap justify-evenly gap-4">
       <goal-card title="Vacation Fund" :target="5000" :days-remaining="45" icon-src="/icons/vacation.svg" :saved="2500"
@@ -22,8 +22,12 @@
         :percentage="50" />
       <goal-card title="Vacation Fund" :target="5000" :days-remaining="45" icon-src="/icons/vacation.svg" :saved="2500"
         :percentage="50" />
-      <EmptyCard />
+      <EmptyCard @click="showCreateGoalForm = true"/>
     </div>
+    <CreateGoal 
+    v-if="showCreateGoalForm" 
+      @close="showCreateGoalForm = false"
+    />
   </div>
 
 </template>
@@ -31,6 +35,7 @@
 
 import AddBtn from '../components/dashboard/addBtn.vue';
 import SearchBare from '../components/dashboard/searchBare.vue';
+import CreateGoal from '../components/goals/CreateGoal.vue';
 import EmptyCard from '../components/goals/emptyCard.vue';
 import GoalCard from '../components/goals/goalCard.vue';
 
@@ -40,7 +45,18 @@ export default {
     GoalCard,
     EmptyCard,
     AddBtn,
-    SearchBare
+    SearchBare,
+    CreateGoal
+  },
+  data(){
+    return {
+      showCreateGoalForm: false,
+    }
+  },
+  methods : {
+    addGoal() {
+      this.showCreateGoalForm = false;
+    }
   }
 }
 </script>
