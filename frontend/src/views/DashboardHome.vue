db637bd0b4cccfeb1510d9a32de810b658a5a09c
anouar el barry <elbarryanwar37@gmail.com>
2025-04-12 17:42:52 +0100

    Update DashboardHome layout and card display

---
+++ frontend/src/views/DashboardHome.vue
@@ -0,0 +1,38 @@
+<template>
+        <div class="cards gap-2 flex flex-wrap w-full">
+          <Dash_card 
+           :value="3000"
+           title="Income"
+           
+          />
+          <Dash_card 
+           :value="3000"
+           title="Income"
+           
+          />
+          <Dash_card 
+           :value="3000"
+           title="Income"
+           
+          />
+          <Dash_card 
+           :value="3000"
+           title="Income"
+           
+          />
+        </div>
+</template>
+
+<script>
+
+import Dash_card from './Dash_card.vue';
+
+export default {
+    name: 'DashboardHome',
+    components : {
+        Dash_card
+    }
+   
+}
+
+</script>
