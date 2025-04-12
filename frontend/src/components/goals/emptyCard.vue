c7400eb40b9b54677421c3eff639d088345c703d
anouar el barry <elbarryanwar37@gmail.com>
2025-04-12 21:07:33 +0100

    Update EmptyCard component styling for consistent sizing

--- frontend/src/components/goals/emptyCard.vue
+++ frontend/src/components/goals/emptyCard.vue
@@ -1,14 +1,14 @@
 <template>
 <button
      
-      class="bg-gray-50 border-gray-300 flex w-[302px] max-w-full flex-col items-center text-base text-gray-600 font-medium mt-6 px-[60px] py-[68px] rounded-xl border-dashed border-2 max-md:px-5"
+      class="bg-gray-50 border-gray-300 flex min-w-2xs max-h-60  flex-col items-center text-base text-gray-600 font-medium px-[60px] py-[68px] rounded-xl border-dashed border-2 max-md:px-5"
     >
       <img
         src="https://cdn.builder.io/api/v1/image/assets/f0075fd21e26474ca5006c398c2a8c3a/0a7ab503b6d3f4b9292f8e77dbae8bf5513967494d0c4d5ba3d8f7f57641cf72?placeholderIfAbsent=true"
         class="aspect-[0.87] object-contain w-[26px]"
         alt="Plus icon"
       />
-      <div class="bg-[rgba(0,0,0,0)] w-[102px] max-w-full mt-2 pt-0.5 pb-2.5 px-px">
+      <div class="bg-[rgba(0,0,0,0)] w-full max-w-full mt-2 pt-0.5 pb-2.5 px-px">
         Add New Goal
       </div>
     </button>
