635135df5f9edf282a07ec5e485bd44acc98ae03
anouar el barry <elbarryanwar37@gmail.com>
2025-04-12 21:07:43 +0100

    Add new dashboard components: AddBtn and SearchBare

---
+++ frontend/src/components/dashboard/addBtn.vue
@@ -0,0 +1,19 @@
+<template>
+     <button
+      class="bg-[rgba(8,16,33,1)] border text-[13px] text-white px-[23px] py-[17px] rounded-md border-solid max-md:px-5"
+    >
+      {{ name }}
+    </button>
+</template>
+
+<script>
+export default {
+    name : 'AddBtn',
+    props : {
+        name : {
+           type : String,
+           default :  'Add income'
+        }
+    }
+}
+</script>
