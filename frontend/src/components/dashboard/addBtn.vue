66a1de6ecddbb4753781c9ab0f090f42f105eaa2
anouar el barry <elbarryanwar37@gmail.com>
2025-04-12 21:56:21 +0100

    Refactor AddBtn component styling and add cursor pointer

--- frontend/src/components/dashboard/addBtn.vue
+++ frontend/src/components/dashboard/addBtn.vue
@@ -1,6 +1,6 @@
 <template>
      <button
-      class="bg-[rgba(8,16,33,1)] border text-[13px] text-white px-[23px] py-[17px] rounded-md border-solid max-md:px-5"
+      class="bg-[rgba(8,16,33,1)] cursor-pointer border text-3 text-white px-4 py-3 rounded-md border-solid max-md:px-5"
     >
       {{ name }}
     </button>
