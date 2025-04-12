6d84f2cb22f5209a315657a1f57a51b3dc37dada
anouar el barry <elbarryanwar37@gmail.com>
2025-04-12 21:07:30 +0100

    Enhance Goals page layout with SearchBare and AddBtn components

--- frontend/src/views/Goals.vue
+++ frontend/src/views/Goals.vue
@@ -1,58 +1,39 @@
 <template>
-    <div class="goals_cards flex flex-wrap gap-2">
-        <goal-card 
-          title="Vacation Fund"
-          :target="5000"
-          :days-remaining="45"
-          icon-src="/icons/vacation.svg"
-          :saved="2500"
-          :percentage="50"
-/>
-        <goal-card 
-          title="Vacation Fund"
-          :target="5000"
-          :days-remaining="45"
-          icon-src="/icons/vacation.svg"
-          :saved="2500"
-          :percentage="50"
-/>
-        <goal-card 
-          title="Vacation Fund"
-          :target="5000"
-          :days-remaining="45"
-          icon-src="/icons/vacation.svg"
-          :saved="2500"
-          :percentage="50"
-/>
-        <goal-card 
-          title="Vacation Fund"
-          :target="5000"
-          :days-remaining="45"
-          icon-src="/icons/vacation.svg"
-          :saved="2500"
-          :percentage="50"
-/>
-        <goal-card 
-          title="Vacation Fund"
-          :target="5000"
-          :days-remaining="45"
-          icon-src="/icons/vacation.svg"
-          :saved="2500"
-          :percentage="50"
-/>
-<EmptyCard />
+  <div class="flex flex-col w-full gap-7">
+    <div class="up_content w-full justify-end items-center flex gap-5">
+      <SearchBare />
+      <AddBtn name="Add income" />
     </div>
+    <div class="goals_cards flex flex-wrap justify-evenly gap-4">
+      <goal-card title="Vacation Fund" :target="5000" :days-remaining="45" icon-src="/icons/vacation.svg" :saved="2500"
+        :percentage="50" />
+      <goal-card title="Vacation Fund" :target="5000" :days-remaining="45" icon-src="/icons/vacation.svg" :saved="2500"
+        :percentage="50" />
+      <goal-card title="Vacation Fund" :target="5000" :days-remaining="45" icon-src="/icons/vacation.svg" :saved="2500"
+        :percentage="50" />
+      <goal-card title="Vacation Fund" :target="5000" :days-remaining="45" icon-src="/icons/vacation.svg" :saved="2500"
+        :percentage="50" />
+      <goal-card title="Vacation Fund" :target="5000" :days-remaining="45" icon-src="/icons/vacation.svg" :saved="2500"
+        :percentage="50" />
+      <EmptyCard />
+    </div>
+  </div>
+
 </template>
 <script>
 
+import AddBtn from '../components/dashboard/addBtn.vue';
+import SearchBare from '../components/dashboard/searchBare.vue';
 import EmptyCard from '../components/goals/emptyCard.vue';
 import GoalCard from '../components/goals/goalCard.vue';
 
 export default {
-    name : 'Goals',
-    components : {
-        GoalCard,
-        EmptyCard
-    }
+  name: 'Goals',
+  components: {
+    GoalCard,
+    EmptyCard,
+    AddBtn,
+    SearchBare
+  }
 }
 </script>
