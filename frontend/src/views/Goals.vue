928fd20f9124a0f365d7649c93ae15f85e216652
anouar el barry <elbarryanwar37@gmail.com>
2025-04-12 20:20:17 +0100

    Implement Goals page with multiple goal cards and empty card

---
+++ frontend/src/views/Goals.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="goals_cards flex flex-wrap gap-2">
+        <goal-card 
+          title="Vacation Fund"
+          :target="5000"
+          :days-remaining="45"
+          icon-src="/icons/vacation.svg"
+          :saved="2500"
+          :percentage="50"
+/>
+        <goal-card 
+          title="Vacation Fund"
+          :target="5000"
+          :days-remaining="45"
+          icon-src="/icons/vacation.svg"
+          :saved="2500"
+          :percentage="50"
+/>
+        <goal-card 
+          title="Vacation Fund"
+          :target="5000"
+          :days-remaining="45"
+          icon-src="/icons/vacation.svg"
+          :saved="2500"
+          :percentage="50"
+/>
+        <goal-card 
+          title="Vacation Fund"
+          :target="5000"
+          :days-remaining="45"
+          icon-src="/icons/vacation.svg"
+          :saved="2500"
+          :percentage="50"
+/>
+        <goal-card 
+          title="Vacation Fund"
+          :target="5000"
+          :days-remaining="45"
+          icon-src="/icons/vacation.svg"
+          :saved="2500"
+          :percentage="50"
+/>
+<EmptyCard />
+    </div>
+</template>
+<script>
+
+import EmptyCard from '../components/goals/emptyCard.vue';
+import GoalCard from '../components/goals/goalCard.vue';
+
+export default {
+    name : 'Goals',
+    components : {
+        GoalCard,
+        EmptyCard
+    }
+}
+</script>
