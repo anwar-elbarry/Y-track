efd007e95c1642ee5b5da60c32b687d117abb0dc
anouar el barry <elbarryanwar37@gmail.com>
2025-04-12 21:10:23 +0100

    remove all bldae pages

--- backend/resources/views/layoats/base.blade.php
+++
@@ -1,6 +0,0 @@
-@extends('layoats.app')
-@section('main-content')
-@yield('navbar')
-@yield('base-content')
-@yield('footer')
-@endsection
--- backend/resources/views/pages/base_dashboard.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layoats.app')
-@section('main-content')
-    <div class="relative container flex h-screen w-full">
-    <div class="main_content w-full h-full">
-oudhfiudsgbiufgespdi Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente voluptatum eligendi harum, aliquid amet error officia delectus ex doloremque ad officiis, esse vel, blanditiis omnis iste. Aspernatur ab perspiciatis reprehenderit.
-        </div>
-        @include('components.aside')
-    </div>
-@endsection
