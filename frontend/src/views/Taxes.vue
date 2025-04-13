928e998e7ccfd2806b043e2e0032adbdb509eb79
anouar el barry <elbarryanwar37@gmail.com>
2025-04-13 20:54:45 +0100

    create invoices and taxes pages with the main structure

---
+++ frontend/src/views/Taxes.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="tax_container w-full h-full gap-4">
+        <TaxRecords />
+    </div>
+</template>
+
+<script>
+
+
+import TaxRecords from '../components/Tax/TaxRecords.vue';
+
+export default {
+    name: 'Taxes',
+    components: {
+        TaxRecords
+    },
+}
+</script>
---
+++ frontend/src/views/invoices.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="Invoice_container flex flex-col w-full h-full gap-4">
+    
+        <Invoice_generator />
+         
+    </div>
+</template>
+
+<script>
+
+import Invoice_generator from '../components/Invioce/invoice_generator.vue';
+
+export default {
+    name: 'Invoice',
+    components: {
+        Invoice_generator
+    },
+}
+</script>
