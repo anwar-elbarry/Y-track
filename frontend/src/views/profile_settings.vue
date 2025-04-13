fb0c199f30cd23fe340043b62a5b47b69eabd7c6
anouar el barry <elbarryanwar37@gmail.com>
2025-04-13 19:45:30 +0100

    Create profile settings view

---
+++ frontend/src/views/profile_settings.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="incomes_container w-full h-full gap-4">
+        <profile_settings />
+    </div>
+</template>
+
+<script>
+
+import Profile_settings from '../components/profile/profile_settings.vue';
+
+export default {
+    name: 'ProfileSettings',
+    components: {
+        Profile_settings
+    },
+}
+</script>
