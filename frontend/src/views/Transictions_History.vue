77b186cf310ec4cb41bf599212945430c0dfcf65
anouar el barry <elbarryanwar37@gmail.com>
2025-04-13 17:49:25 +0100

    Create Transactions History view

---
+++ frontend/src/views/Transictions_History.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="incomes_container flex flex-col w-full h-full gap-4">
+        
+        <transictionHistory_table />
+         
+    </div>
+</template>
+
+<script>
+import transictionHistory_table from '../components/transictions/transictionHistory_table.vue';
+
+export default {
+    name: 'Transactions_History',
+    components: {
+        transictionHistory_table
+    }
+}
+</script>
