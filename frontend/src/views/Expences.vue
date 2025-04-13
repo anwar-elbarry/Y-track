7e7d3b285c26ccc28377a99fbb391e07b21c6336
anouar el barry <elbarryanwar37@gmail.com>
2025-04-13 15:48:01 +0100

    create expences page

---
+++ frontend/src/views/Expences.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="Expences_container flex flex-col w-full h-full gap-4">
+        <div class="up_content w-full justify-end items-center flex gap-5">
+            <SearchBare title="Expence" />
+            <AddBtn name="Add Expence"/>
+        </div>
+        <Expence_table />
+    </div>
+</template>
+
+<script>
+import AddBtn from '../components/dashboard/addBtn.vue';
+import SearchBare from '../components/dashboard/searchBare.vue';
+import Expence_table from '../components/expences/expence_table.vue';
+
+export default {
+    name: 'Expences',
+    components: {
+        AddBtn,
+        SearchBare,
+        Expence_table
+    }
+}
+</script>
