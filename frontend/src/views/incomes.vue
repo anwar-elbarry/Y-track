672f0355ade603a662b3b851c70471abf5f1a062
anouar el barry <elbarryanwar37@gmail.com>
2025-04-13 13:57:46 +0100

    create structure of incomes pages with requiring components needed

---
+++ frontend/src/views/incomes.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="incomes_container flex flex-col w-full h-full gap-4">
+        <div class="up_content w-full justify-end items-center flex gap-5">
+      <SearchBare
+       title="Goal" />
+      <AddBtn name="Add Goal" @click="showCreateGoalForm = true"/>
+    </div>
+    <Income_table />
+    </div>
+</template>
+<script>
+
+import AddBtn from '../components/dashboard/addBtn.vue';
+import SearchBare from '../components/dashboard/searchBare.vue';
+import Income_table from '../components/income/income_table.vue';
+
+export default {
+    name : 'Incomes',
+    components : {
+        AddBtn,
+        SearchBare,
+        Income_table
+    }
+}
+
+</script>
