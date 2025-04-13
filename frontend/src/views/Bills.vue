71b68babb462940df51fff9c937889adbb392ce6
anouar el barry <elbarryanwar37@gmail.com>
2025-04-13 17:18:41 +0100

    create bills structure

---
+++ frontend/src/views/Bills.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="Billss_container flex flex-col w-full h-full gap-4">
+        <div class="up_content w-full justify-end items-center flex gap-5">
+            <SearchBare title="Bills" />
+            <AddBtn name="Add Bills" @click="showForm = true"/>
+        </div>
+        
+        <Bills_table />
+    </div>
+</template>
+
+<script>
+import Bills_table from '../components/Bills/Bills_table.vue';
+import AddBtn from '../components/dashboard/addBtn.vue';
+import SearchBare from '../components/dashboard/searchBare.vue';
+
+export default {
+    name: 'Billss',
+    components: {
+        AddBtn,
+        SearchBare,
+        Bills_table
+    }
+}
+</script>
