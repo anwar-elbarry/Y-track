f5d73d608952d79693a363b1d7027d8faa69abf4
anouar el barry <elbarryanwar37@gmail.com>
2025-04-14 17:29:27 +0100

    create notifications page / add the expence form to expence page

--- frontend/src/views/Expences.vue
+++ frontend/src/views/Expences.vue
@@ -2,15 +2,19 @@
     <div class="Expences_container flex flex-col w-full h-full gap-4">
         <div class="up_content w-full justify-end items-center flex gap-5">
             <SearchBare title="Expence" />
-            <AddBtn name="Add Expence"/>
+            <AddBtn @click="openForm = true" name="Add Expence"/>
         </div>
         <Expence_table />
+        <Expence_form 
+        v-if="openForm"
+        @close="closeForm"/>
     </div>
 </template>
 
 <script>
 import AddBtn from '../components/dashboard/addBtn.vue';
 import SearchBare from '../components/dashboard/searchBare.vue';
+import Expence_form from '../components/expences/expence_form.vue';
 import Expence_table from '../components/expences/expence_table.vue';
 
 export default {
@@ -18,7 +22,18 @@ export default {
     components: {
         AddBtn,
         SearchBare,
-        Expence_table
+        Expence_table,
+        Expence_form
+    },
+    data(){
+        return {
+            openForm : false
+        }
+    },
+    methods : {
+        closeForm(){
+            this.openForm = false;
+        }
     }
 }
 </script>
---
+++ frontend/src/views/notifications.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="Invoice_container flex flex-col w-full h-full gap-4">
+    
+        <notification_component />
+         
+    </div>
+</template>
+
+<script>
+
+import notification_component from '../components/notifications/notification.vue';
+
+export default {
+    name: 'notifications',
+    components: {
+        notification_component,
+    },
+}
+</script>
