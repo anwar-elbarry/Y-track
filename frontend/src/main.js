905ca9aac7b732795501b32cdf3eab4f07984f86
anouar el barry <elbarryanwar37@gmail.com>
2025-04-16 11:54:36 +0100

    add icons

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,8 +4,8 @@ import App from './App.vue'
 import router from './router';
 
 import { OhVueIcon, addIcons } from "oh-vue-icons";
-import { FaWallet,BiLinkedin,FaGlobe ,IoArrowForwardCircleOutline, BiCashCoin,BiInstagram ,GiArcheryTarget,MdTimerOutlined, BiCalendarCheckFill,BiPlusSquareDotted ,LaEditSolid,OiTrash, BiEyeFill,RiLineChartLine , GiOnTarget , RiFileTextFill , BiArrowLeft , BiArrowRight} from "oh-vue-icons/icons";
-addIcons(FaWallet,BiLinkedin,FaGlobe ,IoArrowForwardCircleOutline, BiCashCoin ,BiInstagram,GiArcheryTarget,MdTimerOutlined,BiCalendarCheckFill ,BiPlusSquareDotted,LaEditSolid,OiTrash,BiEyeFill, RiLineChartLine , GiOnTarget , RiFileTextFill , BiArrowLeft, BiArrowRight);
+import { FaWallet,BiLinkedin,IoTrendingDownOutline,IoTrendingUpOutline ,FaGlobe,IoArrowForwardCircleOutline,FaFileInvoiceDollar, BiCashCoin,BiInstagram ,GiArcheryTarget,MdTimerOutlined, BiCalendarCheckFill,BiPlusSquareDotted ,LaEditSolid,OiTrash, BiEyeFill,RiLineChartLine , GiOnTarget , RiFileTextFill , BiArrowLeft , BiArrowRight} from "oh-vue-icons/icons";
+addIcons(FaWallet,BiLinkedin,IoTrendingDownOutline,IoTrendingUpOutline,FaGlobe ,IoArrowForwardCircleOutline,FaFileInvoiceDollar, BiCashCoin ,BiInstagram,GiArcheryTarget,MdTimerOutlined,BiCalendarCheckFill ,BiPlusSquareDotted,LaEditSolid,OiTrash,BiEyeFill, RiLineChartLine , GiOnTarget , RiFileTextFill , BiArrowLeft, BiArrowRight);
 
 
 const app = createApp(App);
