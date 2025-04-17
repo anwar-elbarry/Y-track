0bad8d935be59e1613553e45a45f98dfa84d2691
anouar el barry <elbarryanwar37@gmail.com>
2025-04-17 16:25:11 +0100

    update api

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,20 +1,23 @@
+// api.js
 import axios from "axios";
-import { useAuthStore } from '@/stores/auth'
+import useAuthStore from './stores/auth'; 
 
 const api = axios.create({
-    baseURL : 'http://127.0.0.1:8000/api',
-    headers : {
-        'Content-Type' : 'application/json'
-    }
+  baseURL: 'http://127.0.0.1:8000/api',
+  headers: {
+    'Content-Type': 'application/json'
+  }
 });
 
-
+// Add request interceptor
 api.interceptors.request.use((config) => {
-    const authStore = useAuthStore();
-    if(authStore.token){
-        config.headers.Authorization = `Bearer ${authStore.token}`;
-    }
-    return config;
+  const authStore = useAuthStore(); // get the store instance
+  if (authStore.token) {
+    config.headers.Authorization = `Bearer ${authStore.token}`;
+  }
+  return config;
+}, (error) => {
+  return Promise.reject(error);
 });
 
 export default api;
