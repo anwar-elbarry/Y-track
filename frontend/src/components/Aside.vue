4f62f1be95df94691901992ad0d9de28237ea6c1
anouar el barry <elbarryanwar37@gmail.com>
2025-04-17 09:17:54 +0100

    ignore node_modules

--- frontend/src/components/Aside.vue
+++ frontend/src/components/Aside.vue
@@ -1,8 +1,8 @@
 <template>
     <div :class="{ '-translate-x-50': !open }"
-        class="fixed aside bg-white shadow-[0px_12px_33px_rgba(0,0,0,0.07)] flex h-screen max-w-53 flex-col rounded-xl">
+        class="fixed aside bg-white shadow-[0px_12px_33px_rgba(0,0,0,0.07)] flex h-screen max-w-53 flex-col rounded-xl ">
         <!-- up logo -->
-        <div class="absolute -right-4 top-3 z-20 cursor-pointer">
+        <div :class="{'-right-7' : !open}" class="absolute -right-4 top-3 z-20 cursor-pointer">
             <div @click="toggle_aside()"
                 class="aside_flesh bg-orange-500 shadow-md border border-gray-100 w-8 h-8 rounded-full flex items-center justify-center">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2"
