a3fe14c04e90874664144515cc4bbd2326bfb77c
anouar el barry <elbarryanwar37@gmail.com>
2025-04-18 19:37:44 +0100

    add user route

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -31,6 +31,9 @@ Route::prefix('/auth')->group(function(){
     Route::get('/facebook/callback',[FacebookAuthController::class,'callback']);
     // logoutAuth
     Route::middleware('auth:sanctum')->group(function(){
+        Route::get('/user', function (Request $request) {
+            return $request->user();
+        });
         Route::post('/logout',[AuthController::class,'logOut'])->name('logout');
     });    
 });
