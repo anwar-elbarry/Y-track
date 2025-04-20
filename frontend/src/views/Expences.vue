0eaa014ed3aea099c5313a97b486f26810497cb6
anouar el barry <elbarryanwar37@gmail.com>
2025-04-20 16:25:48 +0100

    Add Expenses view with store integration and components

--- frontend/src/views/Expences.vue
+++ frontend/src/views/Expences.vue
@@ -4,10 +4,16 @@
             <SearchBare title="Expence" />
             <AddBtn @click="openForm = true" name="Add Expence"/>
         </div>
-        <Expence_table />
+        <Expence_table 
+        :expenses="expenses"
+        @reload-expenses="fetchExpenses"
+        @selected-expense="openForm = true"
+        />
         <Expence_form 
         v-if="openForm"
-        @close="closeForm"/>
+        @close="closeForm"
+        @reload-expenses="fetchExpenses"
+        />
     </div>
 </template>
 
@@ -16,7 +22,7 @@ import AddBtn from '../components/dashboard/addBtn.vue';
 import SearchBare from '../components/dashboard/searchBare.vue';
 import Expence_form from '../components/expences/expence_form.vue';
 import Expence_table from '../components/expences/expence_table.vue';
-
+import { useExpenseStore } from '../stores/expenseStore';
 export default {
     name: 'Expences',
     components: {
@@ -25,15 +31,29 @@ export default {
         Expence_table,
         Expence_form
     },
+    setup(){
+        const expenseStore = useExpenseStore()
+        return { expenseStore }
+    },
     data(){
         return {
-            openForm : false
+            openForm : false,
+            expenses: []
         }
     },
     methods : {
         closeForm(){
             this.openForm = false;
+        },
+        async fetchExpenses(){
+            await this.expenseStore.fetchExpenses()
+            this.expenses = this.expenseStore.expenses
+            console.log('expenses in table :',this.expenses);
+            
         }
+    },
+    created(){
+        this.fetchExpenses();
     }
 }
 </script>
