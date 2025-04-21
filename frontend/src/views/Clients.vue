321ea0d28fccd8efa247b007797a889dbd17cd66
anouar el barry <elbarryanwar37@gmail.com>
2025-04-21 02:59:35 +0100

    Update clients view with proper data handling

--- frontend/src/views/Clients.vue
+++ frontend/src/views/Clients.vue
@@ -2,7 +2,7 @@
     <div class="Clients_container flex flex-col w-full h-full gap-4">
         <div class="up_content w-full justify-end items-center flex gap-5">
             <SearchBare title="Client" />
-            <AddBtn name="Add Client"/>
+            <AddBtn @click="openForm()" name="Add Client"/>
         </div>
         <div class="clients_cards flex flex-wrap justify-evenly gap-4">
             <Client_Card v-for="client in clients"
@@ -10,73 +10,58 @@
               :email="client.email"
               :income="client.income"
               :name="client.name"
-              :number="client.number"
+              :phone="client.phone"
               :instagram="client.instagram"
               :linkedin="client.linkedin"
               :social="client.social"/>
         </div>
+        <Client_form  
+        v-if="isOpen"
+        @close="closeForm"
+        @client-added="fetchClients"
+        />
     </div>
 </template>
 
 <script>
 import Client_Card from '../components/Client.vue/Client_Card.vue';
+import Client_form from '../components/Client.vue/Client_form.vue';
 import AddBtn from '../components/dashboard/addBtn.vue';
 import SearchBare from '../components/dashboard/searchBare.vue';
+import { useClientStore } from '../stores/clientStore';
 
 export default {
     name: 'Clients',
     data(){
         return {
-            clients : [
-            {
-            address: '123 Casablanca St, Morocco',
-            email: 'anouar.elbarry@example.com',
-            income: 5000,
-            name: 'Anouar El Barry',
-            number: '+212 663547463',
-            instagram: 'https://www.linkedin.com/in/anouar-el-barry-39475a254/',
-            linkedin: 'https://www.linkedin.com/in/anouar-el-barry-39475a254/',
-            social: 'https://www.linkedin.com/in/anouar-el-barry-39475a254/'
-        },
-        {
-            address: '123 Casablanca St, Morocco',
-            email: 'anouar.elbarry@example.com',
-            income: 5000,
-            name: 'Anouar El Barry',
-            number: '+212 663547463',
-            instagram: 'https://www.linkedin.com/in/anouar-el-barry-39475a254/',
-            linkedin: 'https://www.linkedin.com/in/anouar-el-barry-39475a254/',
-            social: 'https://www.linkedin.com/in/anouar-el-barry-39475a254/'
-        },
-        {
-            address: '123 Casablanca St, Morocco',
-            email: 'anouar.elbarry@example.com',
-            income: 5000,
-            name: 'Anouar El Barry',
-            number: '+212 663547463',
-            instagram: 'https://www.linkedin.com/in/anouar-el-barry-39475a254/',
-            linkedin: 'https://www.linkedin.com/in/anouar-el-barry-39475a254/',
-            social: 'https://www.linkedin.com/in/anouar-el-barry-39475a254/'
-        },
-                {
-            address: '123 Casablanca St, Morocco',
-            email: 'anouar.elbarry@example.com',
-            income: 5000,
-            name: 'Anouar El Barry',
-            number: '+212 663547463',
-            instagram: 'https://www.linkedin.com/in/anouar-el-barry-39475a254/',
-            linkedin: 'https://www.linkedin.com/in/anouar-el-barry-39475a254/',
-            social: 'https://www.linkedin.com/in/anouar-el-barry-39475a254/'
-        },
-            ]
-            
+            clients : [],
+            isOpen : false,
             }
-           
     },
+    setup(){
+        const clientStore = useClientStore();
+        return {clientStore};
+     },
     components: {
         AddBtn,
         SearchBare,
-        Client_Card
+        Client_Card,
+        Client_form
+    },
+    methods : {
+        openForm(){
+            this.isOpen = true;
+        },
+        closeForm(){
+            this.isOpen = false;
+        },
+       async fetchClients(){
+            await this.clientStore.fetchClients();
+            this.clients = this.clientStore.clients;
+        }
+    },
+    created(){
+        this.fetchClients();
     }
 }
 </script>
