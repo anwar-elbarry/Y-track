7d934f0eda1598028dd89d5d82d7677962a4b412
anouar el barry <elbarryanwar37@gmail.com>
2025-04-22 01:56:19 +0100

    add: implement Bills form and category form with data fetching and state management

--- frontend/src/views/Bills.vue
+++ frontend/src/views/Bills.vue
@@ -3,9 +3,22 @@
         <div class="up_content w-full justify-end items-center flex gap-5">
             <SearchBare title="Bills" />
             <AddBtn name="Add Bills" @click="showForm = true"/>
+            <AddBtn name="Add Category" @click="showCategoryForm = true"/>
         </div>
         
-        <Bills_table />
+        <Bills_table 
+        :BillItems="bills"/>
+        <BillForm 
+        v-if="showForm"
+        :billsCategories="billCategories"
+        @close="closeForm"
+        @bill-added="fetchBills"
+        />
+        <Bills_category_form 
+        v-if="showCategoryForm"
+        @close="closeCategoryForm"
+        @category-added="fetchBillCategories"
+        />        
     </div>
 </template>
 
@@ -13,13 +26,60 @@
 import Bills_table from '../components/Bills/Bills_table.vue';
 import AddBtn from '../components/dashboard/addBtn.vue';
 import SearchBare from '../components/dashboard/searchBare.vue';
+import BillForm from '../components/Bills/Bills_Form.vue';
+import Bills_category_form from '../components/Bills/Bills_category_form.vue';
+import { useBillStore } from '../stores/billsStore';
+import { useBillCategoryStore } from '../stores/billsCategoryStore';
 
 export default {
     name: 'Billss',
+    data(){
+        return{
+            showForm:false,
+            billCategories:[],
+            bills:[],
+            showCategoryForm : false,
+        }
+    },
+    setup(){
+        const billStore = useBillStore();
+        const billCategoryStore = useBillCategoryStore();
+        return { billStore, billCategoryStore };
+    },
     components: {
         AddBtn,
         SearchBare,
-        Bills_table
+        Bills_table,
+        BillForm,
+        Bills_category_form
+    },
+    mounted(){
+        this.fetchBills()
+    },
+    methods:{
+      async fetchBills(){
+            await this.billStore.fetchBills();
+            this.bills = this.billStore.bills;
+            await this.fetchBillCategories();
+        },
+        async fetchBillCategories(){
+            await this.billCategoryStore.fetchCategories();
+            this.billCategories = this.billCategoryStore.categories;
+        },
+        closeForm(){
+            this.showForm = false;
+            this.fetchBills();
+        },
+        closeCategoryForm(){
+            this.showCategoryForm = false;
+            this.fetchBillCategories();
+        },
+        openCategoryForm(){
+            this.showCategoryForm = true;
+        }
+    },
+    created(){
+        this.fetchBills();
     }
 }
 </script>
