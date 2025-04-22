ba88b17a8f47406d8b82531b7668250e2f2bd09f
anouar el barry <elbarryanwar37@gmail.com>
2025-04-22 15:14:57 +0100

    feat: bind transaction items to transictionHistory_table and implement fetchTransactions method

--- frontend/src/views/Transictions_History.vue
+++ frontend/src/views/Transictions_History.vue
@@ -1,18 +1,37 @@
 <template>
     <div class="incomes_container flex flex-col w-full h-full gap-4">
         
-        <transictionHistory_table />
+        <transictionHistory_table 
+        :transacrion-items="transactions"/>
          
     </div>
 </template>
 
 <script>
 import transictionHistory_table from '../components/transictions/transictionHistory_table.vue';
-
+import { useTransactionStore } from '../stores/transactionStore';
 export default {
     name: 'Transactions_History',
     components: {
         transictionHistory_table
+    },
+    setup(){
+        const transactionStore = useTransactionStore();
+        return {transactionStore}
+    },
+    data(){
+        return {
+            transactions : []
+        }
+    },
+    methods : {
+        async fetchTransactions(){
+           await this.transactionStore.fetchtransactions();
+            this.transactions = this.transactionStore.transactions;
+        }
+    },
+    created(){
+        this.fetchTransactions();
     }
 }
 </script>
