c70ffeddc3711ec07b34f0a10d6247666db284e8
anouar el barry <elbarryanwar37@gmail.com>
2025-04-23 10:05:35 +0100

    feat: implement goal store with CRUD actions for managing goals

---
+++ frontend/src/stores/goalStore.js
@@ -0,0 +1,54 @@
+import { defineStore } from 'pinia';
+import api from '../api';
+
+export const useGoalStore = defineStore('goal',{
+    state:()=>({
+        goals:[]
+    }),
+    actions:{
+        async fetchgoals(){
+            try{
+                const response = await api.get('/api/goal/index')
+                this.goals = response.data.goals
+                console.log(response.data.goals);
+                console.log(response.data.message);
+            }catch(error){
+                console.error('Error fetching goals:', error);
+            }
+        },
+        async addgoal(goalData){
+            try{
+                console.log(goalData);
+                
+                const response = await api.post('/api/goal/create',goalData)
+                this.goals.push(response.data.goal)
+                console.log(response.data.goal);
+                console.log(response.data.message);
+            }catch(error){
+                console.error('Error adding goal:', error);
+            }
+        },
+        async removegoal(id){
+            try{
+                await api.delete(`/api/goal/remove/${id}`)
+                this.goals = this.goals.filter(goal => goal.id !== id)
+                console.log(response.data.message);
+            }catch(error){
+                console.error('Error removing goal:', error);
+            }
+        },
+        async updategoal(id,goalData){
+            try{
+                const response = await api.put(`/api/goal/update/${id}`,goalData)
+                const index = this.goals.findIndex(goal => goal.id === id)
+                if(index !== -1){
+                    this.goals[index] = response.data.goal
+                }
+                console.log(response.data.goal);
+                console.log(response.data.message);
+            }catch(error){
+                console.error('Error updating goal:', error);
+            }
+        }
+    }
+})
