e8cd555ea1fb98f207203d836107395cdd7f304b
anouar el barry <elbarryanwar37@gmail.com>
2025-04-24 21:01:51 +0100

    feat: add GenerateTransactionJob to handle income transaction creation and scheduling

---
+++ backend/app/Jobs/GenerateTransactionJob.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Jobs;
+
+use Carbon\Carbon;
+use App\Models\Income;
+use App\Models\Transaction;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class GenerateTransactionJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected int $incomeId;
+    /**
+     * Create a new job instance.
+     */
+    public function __construct(int $incomeId)
+    {
+        $this->incomeId = $incomeId;
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        $income = Income::find($this->incomeId);
+        if(!$income || $income->status === 'inactive'){
+                return ;
+        }
+
+        $today = Carbon::today();
+
+        if($today->lt($income->next_run_at)){
+            return;
+        }
+
+        Transaction::create([
+            'user_id'   => $income->user_id,
+            'income_id' => $income->id,
+            'amount'    => $income->amount,
+            'type'    => 'income',
+        ]);
+
+        $next = match($income->frequency){
+            'daily'    => Carbon::parse($income->next_run_at)->addDay(),
+            'weekly'   => Carbon::parse($income->next_run_at)->addWeek(),
+            'monthly'  => Carbon::parse($income->next_run_at)->addMonth(),
+            'yearly'   => Carbon::parse($income->next_run_at)->addYear(),
+            'one-time'    =>  null,
+            default    =>  null
+        };
+
+        if($next !== null){
+            $income->next_run_at = $next;
+            $income->save();
+        
+            self::dispatch($income->id)
+            ->delay($next->startOfDay());
+        }else {
+            $income->status = 'inactive';
+            $income->save();
+        }
+            }
+
+}
