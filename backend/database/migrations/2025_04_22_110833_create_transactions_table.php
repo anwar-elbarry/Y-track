684e97ac30d8b6d79558a37538cace88f57d37c4
anouar el barry <elbarryanwar37@gmail.com>
2025-04-24 21:00:14 +0100

    feat: add jobs migration to create jobs table with necessary fields

--- backend/database/migrations/2025_04_22_110833_create_transactions_table.php
+++ backend/database/migrations/2025_04_22_110833_create_transactions_table.php
@@ -14,9 +14,9 @@ return new class extends Migration
         Schema::create('transactions', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->nullable()->constrained()->onDelete('cascade');
-            $table->foreignId('income_id')->nullable()->constrained('incomes')->onDelete('cascade');
-            $table->foreignId('expense_id')->nullable()->constrained('expenses')->onDelete('cascade');
-            $table->foreignId('bill_id')->nullable()->constrained('bills')->onDelete('cascade');
+            $table->foreignId('income_id')->nullable()->default(null)->constrained('incomes')->onDelete('cascade');
+            $table->foreignId('expense_id')->nullable()->default(null)->constrained('expenses')->onDelete('cascade');
+            $table->foreignId('bill_id')->nullable()->default(null)->constrained('bills')->onDelete('cascade');
             $table->enum('type', [
                 'income', 
                 'expense', 
