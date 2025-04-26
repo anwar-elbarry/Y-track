6c4ae6654919a70b1bb6d45d959bd3f789c68f4b
anouar el barry <elbarryanwar37@gmail.com>
2025-04-26 23:40:05 +0100

    feat: pass clients data to Invoice_generator and implement client fetching logic

--- frontend/src/views/invoices.vue
+++ frontend/src/views/invoices.vue
@@ -1,7 +1,8 @@
 <template>
     <div class="Invoice_container flex flex-col w-full h-full gap-4">
     
-        <Invoice_generator />
+        <Invoice_generator
+        :clients="clients" />
          
     </div>
 </template>
@@ -9,11 +10,31 @@
 <script>
 
 import Invoice_generator from '../components/Invioce/invoice_generator.vue';
-
+import { useClientStore } from '../stores/clientStore';
 export default {
     name: 'Invoice',
     components: {
         Invoice_generator
     },
+    setup(){
+        const clientStore = useClientStore();
+        return {clientStore};
+    },
+    data(){
+       return {
+        clients : [],
+       } 
+    },
+    methods :{
+       async fetchClients(){
+           await this.clientStore.fetchClients();
+           this.clients = this.clientStore.clients;
+           console.log(this.clients);
+           
+        }
+    },
+    created(){
+        this.fetchClients();
+    }
 }
 </script>
