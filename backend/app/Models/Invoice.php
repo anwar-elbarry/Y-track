48dd3293b0f6b2116e7d6725f45cb485c418f4da
anouar el barry <elbarryanwar37@gmail.com>
2025-04-27 00:09:54 +0100

    fix: correct class name casing and reformat Invoice model structure

--- backend/app/Models/Invoice.php
+++ backend/app/Models/Invoice.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class invoice extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'invoice_path',
+        'client_id',
+        'service_price',
+        'due_date',
+        'service_name',
+        'service_description',
+        'user_id',
+    ];
+    public function client(){
+        return $this->belongsTo(Client::class);
+    }
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
 }
