716565d0c4beca18223454590d192b8886b4922c
anouar el barry <elbarryanwar37@gmail.com>
2025-04-30 10:20:05 +0100

    feat: update DashboardHome to display dynamic statistics for expenses, bills, and incomes

--- frontend/src/views/DashboardHome.vue
+++ frontend/src/views/DashboardHome.vue
@@ -1,4 +1,5 @@
 <template>
+    <div class="dashboard-home">
         <div class="cards gap-10 flex flex-wrap justify-center w-full">
           <Dash_card 
            :value="balance"
@@ -6,39 +7,45 @@
            title="Balance"
           />
           <Dash_card 
-           :value="3000"
+           :value="statistics.totalExpenses"
            title="Expenses"
            icon="io-trending-down-outline"
            
           />
           <Dash_card 
-           :value="3000"
+           :value="statistics.totalBills"
            title="Bills"
            icon="bi-calendar-check-fill"
            
           />
           <Dash_card 
-           :value="3000"
+           :value="statistics.totalIncomes"
            title="Total incomes"
            icon="io-trending-up-outline"
 
           />
         </div>
+        <charts :transactions="transactions" />
+    </div>
 </template>
 
 <script>
-
+import transaction from '../services/transaction';
 import Dash_card from '../components/dashboard/Dash_card.vue';
+import charts from '../components/dashboard/charts.vue';
 import auth from '../stores/auth';
 const authStore = auth();
 export default {
     name: 'DashboardHome',
     components : {
-        Dash_card
+        Dash_card,
+        charts
     },
     data(){
       return {
         balance : 0,
+        statistics : {},
+        transactions : []
       }
     }
    ,
@@ -48,10 +55,20 @@ export default {
             this.balance = authStore.user.balance;
             console.log(this.balance);
             
+      },
+      async getStatistics(){
+            await authStore.getStatistics();
+            this.statistics = authStore.statistics;
+      },
+      async getTransactions(){
+        const data = await transaction.getAll();
+        this.transactions = data.transactions
       }
    },
    created(){
         this.getUser();
+        this.getStatistics();
+        this.getTransactions();
    }
 }
 
