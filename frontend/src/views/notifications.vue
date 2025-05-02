11e3c32c40e7242ef2f609c970e7e6510de30d61
anouar el barry <elbarryanwar37@gmail.com>
2025-05-02 21:05:03 +0100

    feat: enhance notifications view to manage notifications state and unread count

--- frontend/src/views/notifications.vue
+++ frontend/src/views/notifications.vue
@@ -1,7 +1,10 @@
 <template>
     <div class="Invoice_container flex flex-col w-full h-full gap-4">
     
-        <notification_component />
+        <notification_component 
+        :notifications="notifications"
+        :unreadCount="unreadCount"
+        />
          
     </div>
 </template>
@@ -9,11 +12,36 @@
 <script>
 
 import notification_component from '../components/notifications/notification.vue';
+import { useNotificationStore } from '../stores/notificationStore';
 
 export default {
     name: 'notifications',
     components: {
         notification_component,
     },
+    setup() {
+        const notificationStore = useNotificationStore();
+        return {
+            notificationStore
+        }
+    },
+    data() {
+        return {
+            notifications: []
+        }
+    },
+    mounted() {
+        this.fetchNotifications();
+    },
+    methods: {
+        async fetchNotifications() {
+            await this.notificationStore.fetchNotifications();
+            this.notifications = this.notificationStore.notifications;
+            this.unreadCount = this.notificationStore.unreadCount;
+        }
+    },
+    created(){
+        this.fetchNotifications();
+    }
 }
 </script>
