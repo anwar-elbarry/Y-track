046fa9b011227d6aaf3844e9bcc89614d64d31d1
anouar el barry <elbarryanwar37@gmail.com>
2025-05-03 16:57:27 +0100

    feat: add clearAll method to notificationService for clearing all notifications

--- frontend/src/services/notificationService.js
+++ frontend/src/services/notificationService.js
@@ -18,6 +18,20 @@ const notificationService = {
             console.error('Error marking notification as read:', error);
         }
     },
+    async removeNotif(notificationId) {
+        try {
+            await api.delete(`/api/notifications/remove/${notificationId}`);
+        } catch (error) {
+            console.error('Error remove notification:', error);
+        }
+    },
+    async clearAll() {
+        try {
+            await api.delete(`/api/notifications/clear-all`);
+        } catch (error) {
+            console.error('Error clear notifications:', error);
+        }
+    }
 };
 
 export default notificationService;
