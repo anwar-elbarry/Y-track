375c16e2dc7a231bbbcdd6e464c65e2434c478a3
anouar el barry <elbarryanwar37@gmail.com>
2025-05-04 15:26:51 +0100

    fix: update Dash_card component to conditionally style value based on its positivity

--- frontend/src/components/dashboard/Dash_card.vue
+++ frontend/src/components/dashboard/Dash_card.vue
@@ -6,7 +6,7 @@
         <span class="text-sm font-medium">{{ title }}</span>
     </div>
     <div class="flex items-center justify-center gap-2" >
-        <p class="text-l font-bold text-gray-800">
+        <p class="text-l font-bold text-gray-800" :class="value > 0?'text-gray-800':'text-red-500'">
         {{ value }}
     </p>
     <p v-if="title != 'Clients'" class="text-xs font-bold text-gray-400">
