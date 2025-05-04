e066e192136b6d0821bfb0623d3b1a09db7b8d1b
anouar el barry <elbarryanwar37@gmail.com>
2025-05-04 15:28:05 +0100

    fix: update label from 'Reports & Analysis' to 'Invoice & Clients' in Aside component

--- frontend/src/components/Aside.vue
+++ frontend/src/components/Aside.vue
@@ -122,7 +122,7 @@
                                     d="M13.5 10.5H21A7.5 7.5 0 0 0 13.5 3v7.5Z" />
                             </svg>
 
-                            <p class="  font-medium flex-1">Reports & Analysis</p>
+                            <p class="  font-medium flex-1">Invoice & Clients</p>
 
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                                 stroke="currentColor" class="flesh size-3">
