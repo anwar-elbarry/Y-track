dfcb73e41bc6b6927f3f36a2fbeda2f2769cc134
anouar el barry <elbarryanwar37@gmail.com>
2025-05-05 09:45:04 +0100

    feat: add 'status' attribute to users table for user state management

--- backend/database/migrations/2014_10_12_000000_create_users_table.php
+++ backend/database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,7 @@ return new class extends Migration {
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->enum('role', ['admin', 'user'])->default('user');
+            $table->enum('status', ['active', 'suspended'])->default('active');
             $table->string('currency')->default('MAD');
             $table->decimal('threshold_alert', 10, 2)->default(1000);
             $table->decimal('balance', 10, 2)->default(0);
